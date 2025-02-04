f29e4a09fbdf2dfad1610094945655db71e2a70d
DANTES0 <kzstar18rus@gmail.com>
2025-02-04 17:30:41 +0400

    Убрал комменты

--- src/server.ts
+++ src/server.ts
@@ -11,12 +11,6 @@ const app = express()
 function main() {
     app.use(express.json())
     app.use('/api/users', userRoutes)
-    // app.use('/api/nokturn', (req: Request, res: Response) => {
-    //   res.status(200).json({
-    //     message: 'success',
-    //   })
-    // })
-    console.log(process.env.PORT)
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
     })
